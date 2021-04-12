4165c1eee5ede845c6ae2724000e4023f69f4ae4
nalcsmn <nlacsamana09@gmail.com>
2021-04-12 21:16:48 +0800

    Add NavBar

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'php/init.php';
 
-$con = new config();
-$con->con();
+// $con = new config();
+// $con->con();
 
 
 ?>
@@ -20,10 +20,30 @@ $con->con();
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <link rel="icon" href="img/favicon.ico" />
+    <title>QuaranTea</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <!-- NavBAR -->
+    <div class="container-fluid navcon">
+      <div class="container">
+        <nav class="navbar navbar-expand-md   pt-md-5 mb-5">
+          <img src="img/logo.png" class="img-fluid logo" />
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+              <a class="nav-item nav-link active navitem" href="#Home">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link navitem" href="#FoodChoice">Gallery</a>
+              <a class="nav-item nav-link navitem" href="#ourchef">About Us</a>
+              <a class="nav-item nav-link navitem" href="#ourchef">Review</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
