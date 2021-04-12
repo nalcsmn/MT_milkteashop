5096615096ce5b2bcaeacc4e401f7a301da54ae5
nalcsmn <nlacsamana09@gmail.com>
2021-04-12 21:32:33 +0800

    Add logo

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ require_once 'php/init.php';
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="icon" href="img/favicon.ico" />
-    <title>QuaranTea</title>
+    <title>Maneila Tea</title>
   </head>
   <body>
     <!-- NavBAR -->
